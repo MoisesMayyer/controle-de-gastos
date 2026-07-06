9de8451b54458ec5453db01d321e41af9b9d49c7
Mayyer <moisesmayer769@gmail.com>
2026-07-06 19:51:34 -0300

    feat: interação inicial do menu

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from src.menu import iniciar
+
+if __name__ == "__main__":
+    iniciar()
+
---
+++ src/menu.py
@@ -0,0 +1,43 @@
+from gastos import (
+    adicionar_gastos,
+    listar_gastos,
+    editar_gastos,
+    remover_gastos,
+    total_gastos,
+)
+
+def menu():
+    print("="*5, "controle de gastos", "="*5)
+    print(
+        "1 - Adicionar gasto\n"
+        "2 - Listar gastos\n"
+        "3 - Editar gasto\n"
+        "4 - Remover gasto\n"
+        "5 - Total gasto\n"
+        "6 - Sair"
+    )
+    
+
+def iniciar():
+    while True:
+
+        menu()
+
+        try:
+            opcao = int(input("Escolha uma opção: "))
+        except ValueError:
+            print("Digite apenas numeros")
+            continue
+
+        if opcao == 1:
+            adicionar_gastos()
+        elif opcao == 2:
+            listar_gastos()
+        elif opcao == 3:
+            editar_gastos()
+        elif opcao == 4:
+            remover_gastos()
+        elif opcao == 5:
+            total_gastos()
+        else:
+            break
