d72f3cff8bd9ff07c41e9602850cafaeeacc8a0e
Mayyer <moisesmayer769@gmail.com>
2026-07-06 19:52:02 -0300

    feat: adição da estrutura da logica

---
+++ src/gastos.py
@@ -0,0 +1,16 @@
+def adicionar_gastos():
+    print("Adicionando gastos")
+
+def listar_gastos():
+    print("Listando gastos")
+
+
+def editar_gastos():
+    print("Editar gastos")
+
+
+def remover_gastos():
+    print("Remover gastos")
+
+def total_gastos():
+    print("Total gastos")
