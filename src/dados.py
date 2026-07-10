54a3c178262f9660ea8e95a4ed2ef18a17edc54e
Mayyer <moisesmayer769@gmail.com>
2026-07-10 16:17:08 -0300

    feat: função salvar_json

--- src/dados.py
+++ src/dados.py
@@ -1,6 +1,15 @@
+import json
+import os
+
 def carregar_json():
     pass
 
-def salvar_json():
-    pass
+def salvar_json(dados):
+    pasta = "dados"
+    arquivo = "dados/gastos.json"
 
+    if not os.path.exists(pasta):
+        os.makedirs(pasta)
+
+    with open(arquivo, "w", encoding="utf-8") as file:
+        json.dump(dados, file, indent=4, ensure_ascii=False)
