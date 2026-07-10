01d9003addc26bc790cf160fa29dfa55d157aeeb
Mayyer <moisesmayer769@gmail.com>
2026-07-10 15:57:03 -0300

    feat: Adiciona menu principal e navegação do sistema

--- src/menu.py
+++ src/menu.py
@@ -1,23 +1,48 @@
 from gastos import (
-    adicionar_gastos,
+    adicionar_gasto,
     listar_gastos,
-    editar_gastos,
-    remover_gastos,
+    editar_gasto,
+    remover_gasto,
     total_gastos,
 )
 
+
 def menu():
-    print("="*5, "controle de gastos", "="*5)
+    print("=" * 5, "controle de gastos", "=" * 5)
     print(
         "1 - Adicionar gasto\n"
         "2 - Listar gastos\n"
-        "3 - Editar gasto\n"
-        "4 - Remover gasto\n"
-        "5 - Total gasto\n"
-        "6 - Sair"
+        "3 - Total gasto\n"
+        "4 - Sair"
     )
-    
-#menu temporário
+
+
+def sub_menu():
+    while True:
+        try:
+            opcao_lista = int(
+                input(
+                    "Remover [1] / Editar [2] / Voltar [3]: "
+                )
+            )
+        except ValueError:
+            print("Digite apenas números.")
+            continue
+
+        if opcao_lista == 1:
+            remover_gasto()
+
+        elif opcao_lista == 2:
+            editar_gasto()
+
+        elif opcao_lista == 3:
+            break
+
+        else:
+            print("Opção inválida!")
+
+
+# menu temporário
 def iniciar():
     while True:
 
@@ -31,14 +56,13 @@ def iniciar():
             continue
 
         if opcao == 1:
-            adicionar_gastos()
+            adicionar_gasto()
+
         elif opcao == 2:
             listar_gastos()
+            sub_menu()
+
         elif opcao == 3:
-            editar_gastos()
-        elif opcao == 4:
-            remover_gastos()
-        elif opcao == 5:
             total_gastos()
         else:
             break
